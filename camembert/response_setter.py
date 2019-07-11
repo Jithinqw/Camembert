cd8ad4651b4a79d3be03089a84867a4430bbbdcf
Jithin Zacharia <jithin.mscis3@iiitmk.ac.in>
2019-07-11 14:28:05 +0530

    Resolving issue with response_setter

--- camembert/response_setter.py
+++ camembert/response_setter.py
@@ -5,7 +5,7 @@ class SuccessResponseManager:
     """A manager middleware for changing the security
         headers in success response"""
 
-    def process_response(self, req, resp, resource, req_succeeded):
+    def process_response(self, req, resp):
         """process_response, It can process successful response and 
         set headers to every header and deletes any headers which are vernerable. 
         Please refer https://www.owasp.org for more details.
