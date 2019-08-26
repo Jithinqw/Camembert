4cebfb323f52757f8dd205548f8c8c27dea13622
Jithin Zacharia <jithin.mscis3@iiitmk.ac.in>
2019-08-26 18:41:51 +0530

    Adding new logger code.

--- camembert/logging.py
+++ camembert/logging.py
@@ -1,19 +1,48 @@
-import logging
+"""Module for maintaing logs for server."""
+import falcon
+import sys, os, logging, uuid
+from datetime import datetime
 
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.FileHandler("test.log"))
-logger.setLevel(logging.INFO)
+
+def setup_logging(log):
+    file_path = sys.modules[__name__].__file__
+    project_path = os.path.dirname(os.path.dirname(file_path))
+    log_location = project_path + "/logs/"
+    if not os.path.exists(log_location):
+        os.makedirs(log_location)
+    current_time = datetime.now()
+    current_date = current_time.strftime("%Y-%m-%d")
+    file_name = current_date + ".log"
+    file_location = log_location + file_name
+
+    logging.basicConfig(filename=file_location, level=logging.DEBUG)
+    with open(file_location, "a+"):
+        pass
+
+    logger = logging.getLogger(__name__)
+    logger.info(log)
+    return None
 
 
-class ResponseLoggerMiddleware(object):
+class ServerLoggingManager:
+
+    """
+        This module is responsible for maintaing 
+        logs in a .log file. These are the currently
+        implemented details for each log entry.
+        1. IP
+        2. Method
+        3. Resource
+        4. Host
+        5. Forwared host
+        6. URL
+        7. Date
+        8. UUID for each request
+        9. Server Response code
+    """
+
     def process_response(self, req, resp, resource, req_succeeded):
-        """process_response is a middleware for logging response.
-        Args:
-            self (object): class instance
-            req (object): request object 
-            res (object): response object
-        
-        Raises:
-            None
-        """
-        logger.info("{0} {1} {2}".format(req.method, req.relative_uri, resp.status[:3]))
+        construct_logger_info = "{0} {1} {2} {3} {4}".format(
+            uuid.uuid4(), datetime.now(), req.method, req.uri, resp.status
+        )
+        setup_logging(construct_logger_info)
